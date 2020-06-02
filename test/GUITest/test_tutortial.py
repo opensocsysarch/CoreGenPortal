b6798d4bc9fab222eea1a253ae7f4ab40e14f716
David Donofrio <ddonofrio@tactcomplabs.com>
2020-06-01 23:52:53 -0700

    Updates to GUI tester to rely less on screenshots, updated README

--- test/GUITest/test_tutortial.py
+++ test/GUITest/test_tutortial.py
@@ -6,6 +6,8 @@ import platform
 import subprocess
 import Xlib.display
 import os
+import argparse
+import re
 
 #Determine OS so we get the correct window manager
 runningOS       = None
@@ -20,7 +22,37 @@ else:
 
 assert (runningOS != None), "Unsupported OS - Darwin, Ubuntu 18.04 supported. found: " + platform.platform()
 
-runningHeadless = False                     # when true you are running this python script using xvfb-run 
+#Set up the parser
+parser = argparse.ArgumentParser(description='GUI Test program for CoreGenPortal')
+parser.add_argument('-l', '--headless',  action='store_true', default=False, required=False, help='Run headless using Xvfb, Ubuntu ONLY')
+parser.add_argument('-p', '--portal_path', required=False,  default='/opt/coregen/bin/CoreGenPortal', help='Path to CoreGenPortal binary - overrides env var COREGENPORTAL_BIN')
+parser.add_argument('-r', '--resolution', required=True, default='Auto', help='Resolution of screen in "X x Y" pixels where you are launching \
+        CoreGenPortal "Auto" will direct the script to attempt to determine your screen resolution.  Multi-monitor environments may have issues')
+
+args = parser.parse_args()
+
+runningHeadless = args.headless             # when true you are running this python script using xvfb-run
+assert( (runningHeadless and (runningOS is Ubuntu)) or (runningHeadless is False)), "Running headless supported only on Ubuntu"
+
+screenRes = args.resolution
+screenResX = 0
+screenResY = 0
+if('Auto' in screenRes):
+    if(runningOS is Darwin):
+        o   = subprocess.run(["system_profiler", "SPDisplaysDataType"], universal_newlines=True, stdout=subprocess.PIPE)  #using the Python 3.6 signature of this call
+        s   = o.stdout.split()
+        assert('Resolution:' in s), "Could not determine screen resolution from System Profiler"
+        screenResX = int(s[s.index('Resolution:')+1])   
+        screenResY = int(s[s.index('Resolution:')+3])
+    if(runningOS is Ubuntu):
+        o = subprocess.run("xdpyinfo",  universal_newlines=True, stdout=subprocess.PIPE)
+        s = o.stdout.split()
+        assert('dimensions:' in s), "Could not determine screen resolution from xdpyinfo"
+        screenResX = int(s[s.index('dimensions:')+1].split('x')[0])
+        screenResX = int(s[s.index('dimensions:')+1].split('x')[1])
+else:
+    screenResX = int(re.split('x | X', args.resolution)[0]) 
+    screenResY = int(re.split('x | X', args.resolution)[1]) 
 
 #Window offset constants
 nbXDelta        = 0                         # X Distance "notebook" image is from window edge - does not include window banner
@@ -30,12 +62,12 @@ elif(runningOS is Ubuntu):
     nbYDelta        = 56                    # Y Distance "notebook" image is from window edge - does not include window banner
 irXDelta        = 312                       # X Distance "IR" image is from window edge - does not include window banner
 irYDelta        = 54                        # Y Distance "IR" image is from window edge - does not include window banner
-newProjDiagX    = 512                       # X Distance Text box in New project diag is from window edge
-newProjDiagY    = 285                       # Y Distance Text box in New project diag is from window top
+newProjDiagX    = (screenResX / 2)          # X Distance Text box in New project diag is from window edge
+newProjDiagY    = (screenResY / 2) - 99     # Y Distance Text box in New project diag is from window top
 newProjTypeX    = newProjDiagX              # Project Type drop-down box - X offset from window edge
 newProjTypeY    = newProjDiagY + 57         # Project Type drop down box - Y offset from window top
 newProjOKX      = newProjDiagX + 154        # New Project OK button location - relative to window edge
-newProjOKY      = newProjDiagY + 160        # New Project OK button location - relative to window top
+newProjOKY      = newProjDiagY + 150        # New Project OK button location - relative to window top
 if(runningOS is Darwin):
     nodesX          = 50                    # "Nodes" tree root in Project Notebook->Modules location - relative to widow edge
     nodesY          = 105                   # "Nodes" tree root in Project Notebook->Modules location - relative to widow top
@@ -43,6 +75,8 @@ elif(runningOS is Ubuntu):
     nodesX          = 23                    # "Nodes" tree root in Project Notebook->Modules location - relative to widow edge
     nodesY          = 123                   # "Nodes" tree root in Project Notebook->Modules location - relative to widow top
 
+cacheDiagCordX  = 355
+cacheDiagCordY  = 220
 cacheNameX      = 200                       # Cache Name text box offset from Cache diag screenshot
 cacheOKX        = 50                        # New Cache Daig OK button.  Relative to cache diag screenshot
 cacheOKY        = 290                       # New Cache Daig OK button.  Relative to cache diag screenshot
@@ -60,6 +94,7 @@ newProjName     = 'TestNewProj' + datetime.datetime.fromtimestamp(time.time()).s
 cacheName       = 'TestCache'
 cacheSets       = '1'
 cacheWays       = '1'
+cacheLineSize   = '64'
 
 
 
@@ -67,32 +102,18 @@ pyautogui.PAUSE = 1     #Pause one second between all GUI commands - this lets d
 
 #launch CoreGenPortal
 print("launching CoreGen... I'll wait 15 sec")
-coreGenP = subprocess.Popen([os.getenv("COREGENPORTAL_BIN","/opt/coregen/bin/CoreGenPortal"),"--maximize"])
+coreGenP = subprocess.Popen([os.getenv("COREGENPORTAL_BIN",args.portal_path),"--maximize"])
 time.sleep(15)
 assert(coreGenP.poll() == None), "Error launching CoreGenPortal"
 
-#Locate known points on CoreGenGUI - we assume CoreGenGUI is 1024x768 on startup
+#Locate known points on CoreGenGUI - we assume CoreGenGUI is full screen on startup.  Resolution determined above
 
-windowTopX = -1
-windowTopY = -1
-nbCord = pyautogui.locateOnScreen(folderName + 'notebook.png')
-IRCord = pyautogui.locateOnScreen(folderName + 'IR.png')
-
-if((nbCord == None) or (IRCord == None)):
-    print ("taking screenshot")
-    pyautogui.screenshot(newProjName + '.png')
-
-assert( ((nbCord != None) or (IRCord != None)) or (runningHeadless == True) ), "Could not locate known areas of GUI"
-
-if(nbCord != None):
-    windowTopX = nbCord.left - nbXDelta   # X coord is unchanged as screencap is from the far left of the window
-    windowTopY = nbCord.top - nbYDelta    # This screencap is 50 pixels from the top of the window
-
-elif(IRCord != None):
-    #we didn't find the first screencap, so lets try the next one
-    windowTopX = IRCord.left - irXDelta  #X cord is 312 pixels from the edge
-    windowTopY = IRCord.top - irYDelta  # y cord is 54 pixels from the top
-
+if(runningOS is Darwin):
+    windowTopX = 0
+    windowTopY = 45
+elif(runningOS is Ubuntu):
+    windowTopX = 0
+    windowTopY = 80
 else:
     assert runningHeadless, "We are setting X,Y as though running headless"
     windowTopX = 0
@@ -131,12 +152,9 @@ pyautogui.move(0,20)                                                            
 pyautogui.click(button='right')                                                             # Right click to display 'Add Node'
 pyautogui.move(5,5)
 pyautogui.click(button='left')                                                              # Left click to select 'Add Node'
-cacheDiagCord = pyautogui.locateOnScreen(folderName + 'CacheDiag.png')                                   # Find the cache diag box
-if(runningHeadless == False):
-    assert (cacheDiagCord != None), "Could not locate cache dialogue box"                       # We don't actually need to find this in Ubuntu, but a good validation check
 
 if(runningOS is Darwin):
-    pyautogui.click( x=(cacheDiagCord.left + cacheNameX), y=cacheDiagCord.top)                  # Click the cache name text box
+    pyautogui.click( x=(cacheDiagCordX + cacheNameX), y=cacheDiagCordY)              # Click the cache name text box
 
 print("Adding new cache: " + cacheName)
 pyautogui.write(cacheName)                                                                  # Name the cache, then tab to sets, then tab to ways
@@ -144,9 +162,12 @@ pyautogui.press('tab')
 pyautogui.write(cacheSets)
 pyautogui.press('tab')
 pyautogui.write(cacheWays)
+pyautogui.press('tab')
+pyautogui.write(cacheLineSize)
+time.sleep(1)
 
 if(runningOS is Darwin):
-    pyautogui.click( x=(cacheDiagCord.left + cacheOKX), y=(cacheDiagCord.top + cacheOKY))   # Press Save
+    pyautogui.click( x=(cacheDiagCordX + cacheOKX), y=(cacheDiagCordY + cacheOKY))   # Press Save
     time.sleep(1)
     pyautogui.hotkey('command', 's')                                                        # Save Project
     time.sleep(1)
